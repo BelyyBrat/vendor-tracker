3262472c0dec64fadfe264d3745d495875bc5016
Andriy Veremyeyev <belyybrat@gmail.com>
2020-03-13 08:59:00 -0700

    Create Vendor and Order classes and their main methods

--- VendorTracker/Models/Order.cs
+++ VendorTracker/Models/Order.cs
@@ -5,5 +5,39 @@ namespace VendorTracker.Models
 {
   public class Order
   {
+    public string Title {get; set;}
+    public string Description {get; set;}
+    public int Price {get; set;}
+    public DateTime Date = new DateTime();
+    public int OrderId {get; } 
+    private static List <Order> _vendorOrders = new List <Order> () {};
+
+    public Order (string title, string description)
+    {
+      Title = title;
+      Description = description;
+      OrderId = _vendorOrders.Count;
+    }
+
+    public static List<Order> GetOrders()
+    {
+      return _vendorOrders;
+    }
+
+    public static void DeleteAllOrders()
+    {
+      _vendorOrders.Clear();
+    }
+
+    public static void DeleteOrder(int number)
+    {
+        _vendorOrders.RemoveAt(number-1);
+    }
+
+    public static Order FindOrder(int number)
+    {
+      return _vendorOrders[number-1];
+    }
+
   }
 }
--- VendorTracker/Models/Vendor.cs
+++ VendorTracker/Models/Vendor.cs
@@ -5,6 +5,38 @@ namespace VendorTracker.Models
 {
   public class Vendor
   {
+    public string Name {get; set;}
+    public string Description {get; set;}
+    public int VendorId {get;}    
+    private static List <Vendor> _allVendors = new List <Vendor> () {};
+
+    public Vendor (string name, string description)
+    {
+      Name = name;
+      Description = description;
+    }
+
+    public static List <Vendor> GetVendors()
+    {
+      return _allVendors;
+    }
+
+    public static void DeleteAllVendors()
+    {
+      _allVendors.Clear();
+    }
+
+    public static void DeleteVendor(int number)
+    {
+      _allVendors.RemoveAt(number-1);
+    }    
+
+    public static Vendor FindVendor(int number)
+    {
+      return _allVendors[number-1];
+    }
+
   }
+
 
 }
